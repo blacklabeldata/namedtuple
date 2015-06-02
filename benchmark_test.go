064bfecf07c32a3763eb5e6c65cf3805c85d5900
eliquious <max.franks@synapse-wireless.com>
2015-06-01 22:43:00 -0500

    Add small tuple benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -17,8 +17,34 @@ func BenchmarkPutField_1(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		builder.PutString("uuid", "0123456789abcdef")
-		builder.PutString("username", "username")
-		builder.PutUint8("age", uint8(25))
+		// builder.PutString("username", "username")
+		// builder.PutUint8("age", uint8(25))
+		builder.reset()
+	}
+}
+
+func BenchmarkSmallTuple(b *testing.B) {
+
+	Image := New("testing", "Image")
+	Image.AddVersion(
+		Field{"url", true, StringField},
+		Field{"title", true, StringField},
+		Field{"width", true, Uint32Field},
+		Field{"height", true, Uint32Field},
+		Field{"size", true, Uint8Field},
+	)
+
+	// create builder
+	buffer := make([]byte, 128)
+	builder := NewBuilder(Image, buffer)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		builder.PutString("url", "a")
+		builder.PutString("title", "b")
+		builder.PutUint32("width", uint32(1))
+		builder.PutUint32("height", uint32(2))
+		builder.PutUint8("size", uint8(0))
 		builder.reset()
 	}
 }
